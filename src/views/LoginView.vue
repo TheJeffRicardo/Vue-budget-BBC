055e42f75826e2268d98961b1a4b1a2d3a45e8fd
TheJeffRicardo <konkemalindi@gmail.com>
2023-01-24 14:41:04 +0200

    'mybudget'

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+    <Login></Login>
+</template>
+<script>
+import Login from '@/components/LoginComp.vue'
+export default {
+    components: {
+        Login
+    }
+}
+</script>
+<style>
+    
+</style>
